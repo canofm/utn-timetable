878a68fbdd572b6ff0590ca65ae74ceed7cdce0b
canofm <canofedericomartin@gmail.com>
2019-01-21 00:59:57 -0300

    Adding custom exceptions for subjects

---
+++ src/domain/exceptions/index.js
@@ -0,0 +1,15 @@
+export class SubjectMustHaveNameException extends Error {
+  constructor(...args) {
+    super(...args);
+    this.message = "Subject must have a name";
+    Error.captureStackTrace(this.SubjectMustHaveCodeException);
+  }
+}
+
+export class SubjectMustHaveCodeException extends Error {
+  constructor(...args) {
+    super(...args);
+    this.message = "Subject must have a code";
+    Error.captureStackTrace(this.SubjectMustHaveCodeException);
+  }
+}
