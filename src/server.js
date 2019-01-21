a997c3ccf9ffb838331d0296a07b3155c1879884
canofm <canofedericomartin@gmail.com>
2019-01-21 00:21:53 -0300

    Adding router

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { connect } from "./db";
+import { router } from "./api";
+import config from "./config";
 
 const app = express();
 app.use(bodyParser.urlencodeed({ extended: true }));
 app.use(bodyParser.json());
+
+/* connecting to mongodb */
 connect();
+
+app.use(config.api.baseUri, router);
 
 export default app;
