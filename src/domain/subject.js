abf84890063ef9abd390bdb2b6e2b825dc89bc4e
canofm <canofedericomartin@gmail.com>
2019-01-21 01:00:18 -0300

    Adding Subject to domain

---
+++ src/domain/subject.js
@@ -0,0 +1,34 @@
+import _ from "lodash";
+import { SubjectMustHaveNameException, SubjectMustHaveCodeException } from "./exceptions";
+
+class Subject {
+  constructor(build) {
+    this.name = build.name;
+    this.code = build.code;
+  }
+
+  static get Builder() {
+    class Builder {
+      name(name) {
+        this.name = name;
+        return this;
+      }
+      code(code) {
+        this.code = code;
+        return this;
+      }
+      build() {
+        if (_.isEmpty(this.name)) {
+          throw new SubjectMustHaveNameException();
+        }
+        if (_.isEmpty(this.code)) {
+          throw new SubjectMustHaveCodeException();
+        }
+        return new Subject(this);
+      }
+    }
+    return Builder;
+  }
+}
+
+export default Subject;
