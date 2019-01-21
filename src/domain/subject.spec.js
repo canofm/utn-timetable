4b9b1ff70bdf9110b6f39cf721879b4a14d261c8
canofm <canofedericomartin@gmail.com>
2019-01-21 02:28:47 -0300

    Adding tests for building a subject

---
+++ src/domain/subject.spec.js
@@ -0,0 +1,26 @@
+import { expect } from "chai";
+import Subject from "./subject";
+
+describe("Subject", () => {
+  describe("while building", () => {
+    const name = "aName";
+    const code = "1234";
+
+    it("must have a name", () => {
+      expect(() => new Subject.Builder().code(code).build()).to.throw();
+    });
+
+    it("must have a code", () => {
+      expect(() => new Subject.Builder().name(name).build()).to.throw();
+    });
+
+    it("should be ok if it has both: name and code", () => {
+      const subject = new Subject.Builder()
+        .name(name)
+        .code(code)
+        .build();
+      expect(subject.name).to.be.equal(name);
+      expect(subject.code).to.be.equal(code);
+    });
+  });
+});
