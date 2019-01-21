1f55d87dc1a837c4fa60893411c6d5516263d995
canofm <canofedericomartin@gmail.com>
2019-01-21 02:28:31 -0300

    Fixing exceptions

--- src/domain/exceptions/index.js
+++ src/domain/exceptions/index.js
@@ -2,7 +2,7 @@ export class SubjectMustHaveNameException extends Error {
   constructor(...args) {
     super(...args);
     this.message = "Subject must have a name";
-    Error.captureStackTrace(this.SubjectMustHaveCodeException);
+    Error.captureStackTrace(this, SubjectMustHaveCodeException);
   }
 }
 
@@ -10,6 +10,6 @@ export class SubjectMustHaveCodeException extends Error {
   constructor(...args) {
     super(...args);
     this.message = "Subject must have a code";
-    Error.captureStackTrace(this.SubjectMustHaveCodeException);
+    Error.captureStackTrace(this, SubjectMustHaveCodeException);
   }
 }
