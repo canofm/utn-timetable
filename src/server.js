ae903d262024e31017af0a7c51dffb6a28a83a8e
canofm <canofedericomartin@gmail.com>
2019-01-22 18:12:32 -0300

    Adding Pretty-Error

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import { connect } from "./db";
 import { router } from "./api";
 import config from "./config";
+import PrettyError from "pretty-error";
 
 const app = express();
 app.use(bodyParser.urlencodeed({ extended: true }));
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 /* connecting to mongodb */
 connect();
 
+new PrettyError().start();
 app.use(config.api.baseUri, router);
 
 export default app;
