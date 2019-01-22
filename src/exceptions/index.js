9b8631c7e02fc42d17b964308fdd6604230dff93
canofm <canofedericomartin@gmail.com>
2019-01-22 00:20:01 -0300

    Handling mongo errors for duplicated key

--- src/exceptions/index.js
+++ src/exceptions/index.js
@@ -19,7 +19,20 @@ export class EntityNotFound extends Error {
     /* eslint-disable-next-line */
     const { id, sarlompa } = args;
     super(...args);
-    this.message = `Entity with id ${id} not found`;
+    this.message = `Entity with id=${id} not found`;
     Error.captureStackTrace(this, SubjectMustHaveCodeException);
   }
 }
+
+export class DuplicatedSubjectException extends Error {
+  constructor(...args) {
+    /* eslint-disable-next-line */
+    const { name, code, sarlompa } = args;
+    super(...args);
+    this.message = `Entity with name=${name} or code=${code} already exists`;
+    Error.captureStackTrace(this, SubjectMustHaveCodeException);
+  }
+}
+
+export const DUPLICATED_KEY_ERROR_CODE = 11000;
+export const MONGO_EXCEPTION = "MongoError";
--- src/schemas/subject.schema.js
+++ src/schemas/subject.schema.js
@@ -1,13 +1,28 @@
 import mongoose from "mongoose";
+import {
+  DuplicatedSubjectException,
+  MONGO_EXCEPTION,
+  DUPLICATED_KEY_ERROR_CODE
+} from "../exceptions";
 
 const subjectSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Subject must have a name"]
+    required: [true, "Subject must have a name."],
+    unique: true
   },
   code: {
     type: String,
-    required: [true, "Subject must have a code"]
+    required: [true, "Subject must have a code."],
+    unique: true
+  }
+});
+
+subjectSchema.post("save", (error, doc, next) => {
+  if (error.name === MONGO_EXCEPTION && error.code === DUPLICATED_KEY_ERROR_CODE) {
+    next(new DuplicatedSubjectException(doc));
+  } else {
+    next();
   }
 });
 
