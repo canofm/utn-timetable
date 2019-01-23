be9d5dbdb31a2f39a242d25f662ed9c00cfdbd93
canofm <canofedericomartin@gmail.com>
2019-01-23 20:15:34 -0300

    Adding coverage with nyc and istanbul

--- src/api/controllers/subject.controller.spec.js
+++ src/api/controllers/subject.controller.spec.js
@@ -4,21 +4,20 @@ import { cleanDb } from "../../test_helpers";
 import app from "../../server";
 
 chai.use(chaiHttp);
-const request = () => chai.request(app);
+const request = chai.request(app);
 
 describe("Subject API", () => {
   beforeEach(() => {
     cleanDb();
   });
+
   describe("GET", () => {
-    it.only("should get all subjects", () => {
-      request()
-        .get("/api/v0/subject/")
-        .then(result => {
-          expect(result).to.have.status(200);
-          expect(result).to.be.json;
-          expect(result.body).to.be.eql([]);
-        });
+    it("should get all subjects", () => {
+      request.get("/api/v0/subject/").then(result => {
+        expect(result).to.have.status(200);
+        expect(result).to.be.json;
+        expect(result.body).to.be.eql([]);
+      });
     });
   });
 });
