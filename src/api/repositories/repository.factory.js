2bc590cf35a51533b5d3596e8c7958eab4abc6aa
canofm <canofedericomartin@gmail.com>
2019-01-25 17:51:48 -0300

    Migrating promises to async for repository

--- src/api/repositories/repository.factory.js
+++ src/api/repositories/repository.factory.js
@@ -13,36 +13,31 @@ class RepositoryFactory {
           });
       }
 
-      getAll() {
+      async getAll() {
         // TODO: Add pagination
         // TODO: Add filters
-        return model.find({}).then(entities => {
-          return entities.map(entity => {
-            return mapper.toDomain(entity);
-          });
-        });
+        const entities = await model.find({});
+        return entities.map(mapper.toDomain);
       }
 
       create(entity) {
         return model.create(entity).then(mapper.toDomain);
       }
 
-      update(id, entity) {
-        return model.findOneAndUpdate({ _id: id }, entity, { new: true }).then(entity => {
-          if (isEmpty(entity)) {
-            throw new EntityNotFound(modelName, id);
-          }
-          return mapper.toDomain(entity);
-        });
+      async update(id, entity) {
+        const updatedEntity = await model.findOneAndUpdate({ _id: id }, entity, { new: true });
+        if (isEmpty(updatedEntity)) {
+          throw new EntityNotFound(modelName, id);
+        }
+        return mapper.toDomain(updatedEntity);
       }
 
-      delete(id) {
-        return model.deleteOne({ _id: id }).then(result => {
-          if (result.deletedCount === 0) {
-            throw new EntityNotFound(modelName, id);
-          }
-          return result;
-        });
+      async delete(id) {
+        const result = await model.deleteOne({ _id: id });
+        if (result.deletedCount === 0) {
+          throw new EntityNotFound(modelName, id);
+        }
+        return result;
       }
     }
     return merge(new CRUDRepository(), overrides);
